85310c8691d0dd34dc29481b1757a7281b990d78
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-04-05 13:39:37 -0700

    validator now working on login page, link for signup page also working as redirect back to login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
   private createForm(): void {
     this.loginForm = this.fb.group ({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required, Validators.minLength(6)]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
