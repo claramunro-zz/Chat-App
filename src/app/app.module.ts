dc80ffe621ef181ab17f08adb0bac57dd3d3ae64
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-04-05 16:16:20 -0700

    added chat-input, chat-message, chatroom-list, chatroom-title-bar & chatroom-window components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ChatComponent } from './pages/chat/chat.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ChatInputComponent } from './pages/chat/components/chat-input/chat-input.component';
+import { ChatroomListComponent } from './pages/chat/components/chatroom-list/chatroom-list.component';
+import { ChatroomTitleBarComponent } from './pages/chat/components/chatroom-title-bar/chatroom-title-bar.component';
+import { ChatMessageComponent } from './pages/chat/components/chat-message/chat-message.component';
+import { ChatroomWindowComponent } from './pages/chat/components/chatroom-window/chatroom-window.component';
 
 
 @NgModule({
@@ -19,7 +24,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     LoginComponent,
     SignupComponent,
     ChatComponent,
-    NavbarComponent
+    NavbarComponent,
+    ChatInputComponent,
+    ChatroomListComponent,
+    ChatroomTitleBarComponent,
+    ChatMessageComponent,
+    ChatroomWindowComponent
   ],
   imports: [
     BrowserModule,
