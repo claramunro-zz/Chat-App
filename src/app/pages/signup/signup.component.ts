8e01bded829d1e0fbc7759f0415320dbf0845ba0
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-04-10 10:46:40 -0700

    added alerts to login & signup pages

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Alert } from 'src/app/classes/alert';
+import { AlertType } from 'src/app/enums/alert-type.enum';
+import { AlertService } from './../../services/alert.service';
+
 
 @Component({
   selector: 'app-signup',
@@ -10,15 +14,15 @@ export class SignupComponent implements OnInit {
 
   public signupForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private alertService: AlertService) {
     this.createForm();
-   }
+  }
 
   ngOnInit() {
   }
 
   private createForm(): void {
-    this.signupForm = this.fb.group ({
+    this.signupForm = this.fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
@@ -27,9 +31,15 @@ export class SignupComponent implements OnInit {
   }
 
   public submit(): void {
-    //  call the auth service
-    const {firstName, lastName, email, password} = this.signupForm.value;
-    console.log(`First Name: ${firstName}, Last Name ${lastName}, Email: ${email}, Password: ${password}`);
+    if (this.signupForm.valid) {
+      //  call the auth service
+      const { firstName, lastName, email, password } = this.signupForm.value;
+      console.log(`First Name: ${firstName}, Last Name ${lastName}, Email: ${email}, Password: ${password}`);
+    }
+    else {
+      const failedSignedAlert = new Alert('Please enter a valid name, email and password. Try again', AlertType.Danger);
+      this.alertService.alerts.next(failedSignedAlert);
+    }
   }
 
 }
